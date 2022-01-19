5dfc37b95c294c3c630da4d8b919814ac589e464
Anthony <anthonyvovo123@gmail.com>
2022-01-19 13:38:03 -0800

    adding grid increase size function

--- scripts/etchasketch.js
+++ scripts/etchasketch.js
@@ -2,11 +2,42 @@ function updateSlider(newGridSize) {
     sliderOutput.innerText = newGridSize;
 }
 
+function expandGrid(currentGridSize, newGridSize) {
+    let difference = newGridSize - currentGridSize;
+    
+    for(let i = 0; i < currentGridSize; i++) // Loop through current row
+    {
+        let currentRow = document.getElementById(`row-${i}`);
+        let squareSize = MAX_CANVAS_SIZE / newGridSize;
+        
+        // Get current amount of children elements, change their size
+        let currentChildrenAmount = currentRow.children.length;
+        for(let j = 0; j < currentChildrenAmount; j++) {
+            let currentEtchSquare = currentRow.children[j];
+            currentEtchSquare.style.width = `${squareSize}px`;
+            currentEtchSquare.style.height = etchSquare.style.width;
+        }
+
+        // Append new elements, set them to same size
+        for(let j = 0; j < difference; j++) {
+            const etchSquare = document.createElement('div');
+            etchSquare.classList.add('etch-square');
+            etchSquare.style.width = `${squareSize}px`;
+            etchSquare.style.height = etchSquare.style.width;
+
+            currentRow.appendChild(etchSquare);
+        }
+
+    }
+
+    // 
+}
+
 function updateGrid(currentGridSize, newGridSize){
     // console.log(currentGridSize);
     // console.log(newGridSize);
     if(currentGridSize < newGridSize) expandGrid(currentGridSize, newGridSize);
-    else if (currentGridSize > newGridSize) decreaseGrid(currentGridSize, newGridSize);
+    else if (currentGridSize > newGridSize) shrinkGrid(currentGridSize, newGridSize);
 }
 
 function createGrid(gridSize) {
